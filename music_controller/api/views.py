49c83c60dd2792550d5f853200194c0b279b2807
Sugam Karki <sugamkarki7058@gmail.com>
2022-07-01 23:02:04 +0545

    stg

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -10,6 +10,18 @@ class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
+class GetRoom(APIView):
+    serializer_class=RoomSerializer
+    lookup_url_kwarg='code'
+
+    def get(self,request,format=None):
+        code=request.GET.get(self.lookup_url_kwarg)
+        if code !=None:
+            room = Room.objects.filter(code=code)
+            if len(room)>0:
+                data=RoomSerializer(room[0]).data
+                data['is_host']=self.request.session.session_key==room[0].host
+                
 
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
